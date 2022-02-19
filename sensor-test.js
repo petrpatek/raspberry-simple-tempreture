62990c28fe4a84974621c2d5d29692146560dd91
Petr Patek <petrpatekml@gmail.com>
2022-02-19 14:53:04 +0100

    ADDED LOG

--- sensor-test.js
+++ sensor-test.js
@@ -1,6 +1,7 @@
 var sensor = require("node-dht-sensor");
 
 sensor.read(11, 4, function (err, temperature, humidity) {
+    console.log("ERRR: ", err)
     if (!err) {
         console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
     }
